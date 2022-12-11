7145ad17b3d37aa293f5f2a68019bd4a470ac7bc
jt1174595239 <70654910+jt1174595239@users.noreply.github.com>
2022-12-11 12:14:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,9 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*lingtigameacc\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*LingTiGameAcc\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git lingtigameacc https://github.com/esirplayground/luci-app-LingTiGameAcc.git' >>feeds.conf.default
-git clone https://github.com/esirplayground/LingTiGameAcc.git
+echo 'src-git LingTiGameAcc https://github.com/esirplayground/luci-app-LingTiGameAcc.git' >>feeds.conf.default
